fd72ce7f84a980852f86ebe7b394d07433fa950b
Bori888 <ilyes.bori8@gmail.com>
2025-10-16 15:16:14 +0200

    hibás összeadás maxint

--- src/main/Szamologep.java
+++ src/main/Szamologep.java
@@ -5,13 +5,19 @@ public class Szamologep {
     public static int osszead(String a, String b) {
         int num1 = Integer.parseInt(a);
         int num2 = Integer.parseInt(b);
-        return num1+num2;
+        return num1 + num2;
     }
-    
+
     public static double osztas(String a, String b) {
         int num1 = Integer.parseInt(a);
         int num2 = Integer.parseInt(b);
-        return  (double)num1/num2;
+        return (double) num1 / num2;
     }
-    
+
+    public static double osztas(int a, int b) {
+        return (double) a / b;
+    }
+
+  
+
 }
--- src/test/SzamologepTest.java
+++ src/test/SzamologepTest.java
@@ -15,87 +15,120 @@ public class SzamologepTest {
     }
 
     private void helyesHasznalat() {
-        testOsszead0_0();
-        testOsszead2_2();
-        testOsszead2_3();
-        testOsztas6_2();
-        testOsztas7_3();
+        testOsszeadAzonos0_0();
+        testOsszeadSzovegesAzonos2_2();
+        testOsszeadKulonbozoSzamok2_3();
+        testOsszeadMaxInt();
 
+        testOsztasSzovegesKulonbozoek6_2();
+        testOsztasVegesValosok7_3();
+        testOsztasVegtelenValosok5_3();
+        testOsztasEgeszek4_2();
+        testOsztasSzovegesAzonos2_2();
     }
 
     private void hibasHasznalat() {
-        testOsztas7_0();
+        testOsztasSRTNull7_0();
     }
 
-    void testOsszead0_0() {
-        System.out.println("összead: 0+0");
-        int kapott = Szamologep.osszead("0", "0");
+    // Segédfüggvény az egységes kiíráshoz
+    void kiir(String tesztNev, String kifejezes, Object eredmeny) {
+        System.out.println(tesztNev + ":");
+        System.out.println();
+        System.out.printf("                 %s -->%s\n\n", kifejezes, eredmeny.toString());
+    }
+
+    void testOsszeadAzonos0_0() {
+        String a = "0";
+        String b = "0";
+        int kapott = Szamologep.osszead(a, b);
         int vart = 0;
+        kiir("testOsszeadAzonos0_0", a + "+" + b, kapott);
         assert kapott == vart : "hibás összeadás";
     }
 
-    void testOsszead2_2() {
+    void testOsszeadSzovegesAzonos2_2() {
         String a = "2";
         String b = "2";
-        System.out.printf("összead: %s+%s\n", a, b);
         int kapott = Szamologep.osszead(a, b);
         int vart = 4;
-        String hiba = "hibás összeadás --> %s+%s\n".formatted(a, b);
-        hiba += "várt: %d, kapott: %d\n".formatted(vart, kapott);
-        assert kapott == vart : hiba;
+        kiir("testOsszeadSzovegesAzonos2_2", a + "+" + b, kapott);
+        assert kapott == vart : "hibás összeadás";
     }
 
-    void testOsszead2_3() {
+    void testOsszeadKulonbozoSzamok2_3() {
         String a = "2";
         String b = "3";
-        System.out.printf("összead: %s+%s\n", a, b);
         int kapott = Szamologep.osszead(a, b);
         int vart = 5;
-        String hiba = "hibás összeadás --> %s+%s\n".formatted(a, b);
-        hiba += "várt: %d, kapott: %d\n".formatted(vart, kapott);
-        assert kapott == vart : hiba;
+        kiir("testOsszeadKulonbozoSzamok2_3", a + "+" + b, kapott);
+        assert kapott == vart : "hibás összeadás";
     }
 
-    void testOsztas6_2() {
-    String a = "6";
-    String b = "2";
-    System.out.printf("osztás: %s/%s\n", a, b);
-    double kapott = Szamologep.osztas(a, b);
-    int vart = 3;
-    double elteres = 0.0001;
-    String hiba = "hibás osztás --> %s/%s\n".formatted(a, b);
-    hiba += "várt: %d, kapott: %f\n".formatted(vart, kapott);
-    assert Math.abs(kapott - vart) <= elteres : hiba;
-}
-
-
-    void testOsztas7_0() {
-        String a = "7";
-        String b = "0";
-        System.out.printf("osztás: %s/%s\n", a, b);
-
-//        boolean voltMegfeleloKivetel = false;
-//        try {
-//            Szamologep.osztas(a, b);
-//        } catch (ArithmeticException e) {
-//            voltMegfeleloKivetel = true;
-//        } catch (Exception e) {
-//            voltMegfeleloKivetel = false;
-//        }
-//        assert voltMegfeleloKivetel : "Nem jött kivétel vagy nem megfelelő érkezett";
-assert Double.isInfinite(Szamologep.osztas(a, b)):"nem Infinity";
-
+    void testOsszeadMaxInt() {
+        String a = "" + Integer.MAX_VALUE;
+        String b = "1";
+        // Figyelem! Az osszeadás metódus int visszatérési értékével túlcsordulhat, ezt vedd figyelembe!
+        int kapott = Szamologep.osszead(a, b);
+        long vart = Integer.MAX_VALUE + 1L;
+        kiir("testOsszeadMaxInt", a + "+" + b, kapott);
+        assert kapott == vart : "hibás összeadás (túlcsordulás?)";
     }
-    void testOsztas7_3() {
+
+    void testOsztasSzovegesKulonbozoek6_2() {
+        String a = "6";
+        String b = "2";
+        double kapott = Szamologep.osztas(a, b);
+        int vart = 3;
+        double elteres = 0.0001;
+        kiir("testOsztasSzovegesKulonbozoek6_2", a + "/" + b, kapott);
+        assert Math.abs(kapott - vart) <= elteres : "hibás osztás";
+    }
+
+    void testOsztasVegesValosok7_3() {
         String a = "7";
         String b = "3";
-        System.out.printf("osztás: %s/%s\n", a, b);
         double kapott = Szamologep.osztas(a, b);
-        double vart = 7.0/3;
-        double elteres= 0.001;
-        String hiba = "hibás osztás --> %s/%s\n".formatted(a, b);
-        hiba += "várt: %f, kapott: %f\n".formatted(vart, kapott);
-        assert Math.abs(kapott-vart)<=elteres:hiba;
+        double vart = 7.0 / 3;
+        double elteres = 0.001;
+        kiir("testOsztasVegesValosok7_3", a + "/" + b, kapott);
+        assert Math.abs(kapott - vart) <= elteres : "hibás osztás";
+    }
+
+    void testOsztasVegtelenValosok5_3() {
+        String a = "5";
+        String b = "3";
+        double kapott = Szamologep.osztas(a, b);
+        double vart = 5.0 / 3;
+        double elteres = 0.001;
+        kiir("testOsztasVegtelenValosok5_3", a + "/" + b, kapott);
+        assert Math.abs(kapott - vart) <= elteres : "hibás osztás";
+    }
+
+    void testOsztasEgeszek4_2() {
+        int a = 4;
+        int b = 2;
+        int kapott = (int) Szamologep.osztas(a, b);
+        int vart = 4 / 2;
+        kiir("testOsztasEgeszek4_2", a + "/" + b, kapott);
+        assert kapott == vart : "hibás osztás";
+    }
+
+    void testOsztasSzovegesAzonos2_2() {
+        String a = "2";
+        String b = "2";
+        double kapott = Szamologep.osztas(a, b);
+        int vart = 1;
+        kiir("testOsztasSzovegesAzonos2_2", a + "/" + b, kapott);
+        assert kapott == vart : "hibás osztás";
+    }
+
+    void testOsztasSRTNull7_0() {
+        String a = "7";
+        String b = "0";
+        double kapott = Szamologep.osztas(a, b);
+        kiir("testOsztasSRTNull7_0", a + "/" + b, kapott);
+        assert Double.isInfinite(kapott) : "nem Infinity";
     }
 
 }
